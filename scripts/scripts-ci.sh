81f9197d876e6afc2d8765e7960f237a641c0050
Sankalp Naik <51851043+Sankalp22863@users.noreply.github.com>
2025-09-28 23:04:31 -0400

    Update scripts-ci.sh

--- scripts/scripts-ci.sh
+++ scripts/scripts-ci.sh
@@ -9,14 +9,14 @@ echo "🔎 Linting…"
 
 echo "🧪 Running tests…"
 
-# Check if a conda environment is already active
-if [[ -z "${CONDA_DEFAULT_ENV:-}" ]]; then
-  echo "No conda environment active. Activating 'codesign'..."
-  # Adjust 'myenv' to the environment name you want
-  conda activate codesign
-else
-  echo "✅ Conda environment already active: $CONDA_DEFAULT_ENV"
-fi
+# # Check if a conda environment is already active
+# if [[ -z "${CONDA_DEFAULT_ENV:-}" ]]; then
+#   echo "No conda environment active. Activating 'codesign'..."
+#   # Adjust 'myenv' to the environment name you want
+#   conda activate codesign
+# else
+#   echo "✅ Conda environment already active: $CONDA_DEFAULT_ENV"
+# fi
 
 conda activate codesing
 
